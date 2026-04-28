870d282ac04a56196d756a64844eb48ab8d26269
Ralf Lindberg <ralf.lindberg@gmail.com>
2026-04-28 09:35:28 +0200

    Adds importer program

---
+++ backend/Fornspar.Importer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+
+AnsiConsole.MarkupLine($"Welcome, [blue]First Chosen of the Lord of Light[/]!");
+
+
+
+var command = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What [green]command[/] would you like me to perform?")
+        .AddChoices("Import", "Export", "Exit"));
+  
+switch (command)
+{
+    case "Import":
+        AnsiConsole.MarkupLine($"[green]Importing[/] data...");
+        break;
+    case "Export":
+        AnsiConsole.MarkupLine($"[green]Exporting[/] data...");
+        break;
+    case "Exit":
+        AnsiConsole.MarkupLine($"[red]Exiting[/] the program. Farewell!");
+        break;
+}
+
